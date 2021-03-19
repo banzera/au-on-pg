77c15c77c5105df5a8420b078599b5a36613e99c
Bryan Banz <bryan@5and2studio.com>
2021-03-18 21:12:13 -0500

    Add script to create all the views

---
+++ ddl/views.sql
@@ -0,0 +1,58 @@
+-- Create views
+
+-- for sku form
+  \i queries/qryInventoryCounts.sql
+  \i queries/qryInventoryCountsFilter.sql
+
+  -- dependency of subform 3
+  \i queries/qrySKUOrderHistoryUnion.sql
+
+  \i queries/frm_sku_subform1.sql
+  -- subform2 is backed by tblSKU
+  \i queries/frm_sku_subform3.sql
+
+   -- used by subform4
+  \i queries/qrySKUPOHistAU.sql
+  -- used by subform5
+  \i queries/qrySKUPOHistAU2.sql
+
+  -- used by frmSKUCustInfoSubform1
+  \i queries/frm_sku_cust_info_subform1.sql
+
+  -- used by frmSKUCustInfoSubform2
+  \i queries/frm_sku_cust_info_subform2.sql
+
+-- for orders outstanding report
+  \i queries/qry_order_items_outstanding.sql
+
+-- for preorder form
+  -- for subform1
+  \i queries/frm_preorder_subform1.sql
+
+  -- for subform2
+  \i queries/frm_preorder_subform2.sql
+
+  -- for subform3
+  \i queries/frm_preorder_subform3.sql
+
+  -- for subform4
+  \i queries/frm_preorder_subform4.sql
+
+  -- for subform4
+  \i queries/frm_preorder_subform5.sql
+
+  -- dep of subform 6
+  \i queries/qryPreOrderHistoryUnion.sql
+
+  -- for subform6 AND for subform8
+  \i queries/frm_preorder_subform6.sql
+
+  -- dependency of qryPreOrderPOHistAU2
+  \i queries/qryPreOrderPOHistAU.sql
+
+  -- for subform7
+  \i queries/qryPreOrderPOHistAU2.sql
+
+  -- for subform8
+  \i queries/qryPreOrderSubList.sql
+
